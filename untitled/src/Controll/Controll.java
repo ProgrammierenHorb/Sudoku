4d17334d29593c5ec0d30edf78e25b9a29048dec
Luka21034 <i21034@hb.dhbw-stuttgart.de>
2022-06-03 09:08:15 +0200

    Controll ueberarbeitet V 1.0

--- untitled/src/Controll/Controll.java
+++ untitled/src/Controll/Controll.java
@@ -1,16 +1,22 @@
 package Controll;
 import ADT.SudokuCell;
+import GUI.GUI;
 import Solver.Solver;
 import Generator.Generator;
 
 public class Controll {
+    private GUI theGUI;
+    public Controll(){
+        theGUI = new GUI(this);
+    }
+    public static void main(String[] args){
+        Controll theControll = new Controll();
+    }
+
     Solver SudokuSolver = new Solver();
     Generator SudokuGenerator = new Generator();
     SudokuCell[][] Grid = new SudokuCell[9][9];
     SudokuCell[][] SolvedGrid = new SudokuCell[9][9];
-    public Controll(){
-
-    }
 
     public void CallSudokuSolver(){
         SolvedGrid = SudokuSolver.solve(Grid);
--- untitled/src/GUI/ActionHandler.java
+++ untitled/src/GUI/ActionHandler.java
@@ -1,22 +1,31 @@
 package GUI;
 
+import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class ActionHandler implements ActionListener {
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        if(e.getSource() == Var.buttonExit){
-            System.exit(0);
-        }
-        else if(e.getSource() == Var.buttonNewGame){
-            //Code hier für ein neues Game --> Machen wir verschiedene Schwierigkeiten?
-        }
-        else if(e.getSource() == Var.buttonResume){
-            //Code hier
-        }
-        else if(e.getSource() == Var.buttonStr8ts){
-            //Code hier
-        }
+    JButton buttonExit;
+    JButton buttonNewGame;
+    JButton buttonStr8ts;
+    JButton buttonResume;
+
+    public ActionHandler(JButton buttonExit,JButton buttonNewGame, JButton buttonStr8ts,JButton buttonResume) {
+        this.buttonExit = buttonExit;
+        this.buttonNewGame = buttonNewGame;
+        this.buttonResume = buttonResume;
+        this.buttonStr8ts = buttonStr8ts;
     }
+        @Override
+        public void actionPerformed (ActionEvent e){
+            if (e.getSource() == buttonExit) {
+                System.exit(0);
+            } else if (e.getSource() == buttonNewGame) {
+                //Code hier für ein neues Game --> Machen wir verschiedene Schwierigkeiten?
+            } else if (e.getSource() == buttonResume) {
+                //Code hier
+            } else if (e.getSource() == buttonStr8ts) {
+                //Code hier
+            }
+        }
 }
